8cde2255fba63713d3d2c7a497f23f3d0de21a7e
shreyasaga2011-lang <shreyasaga2011@gmail.com>
2025-09-27 23:48:03 -0500

    pillar flashing

--- pillar_light_trials.gd
+++ pillar_light_trials.gd
@@ -1,20 +1,39 @@
 extends Node
 
 @export var total_buttons := 3
-var activated_count := 0
+@export var next_scene_path := "res://backgroundlvl2.tscn"
 
-	
+var activated_count := 0
+var flash_rect: ColorRect
+
+func _ready():
+	# Create a fullscreen white overlay (transparent at first)
+	flash_rect = ColorRect.new()
+	flash_rect.color = Color(1, 1, 1, 0) # white transparent
+	flash_rect.size = get_viewport().get_visible_rect().size
+	add_child(flash_rect)
 
 func button_activate():
 	activated_count += 1
 	if activated_count >= total_buttons:
-		go_to_next_scene()
+		flash_and_transition()
 
-func go_to_next_scene():
-	get_tree().change_scene("res://NextScene.tscn")
+func _process(delta):
+	if Global.activated_pillars == total_buttons:
+		flash_and_transition()
+
+func flash_and_transition():
+	# Prevent double running
+	if not is_instance_valid(flash_rect) or flash_rect.color.a > 0:
+		return
 	
-func _process(delta) -> void:
-	if(Global.activated_pillars == 3):
-		get_tree().change_scene_to_file("res://backgroundlvl2.tscn")
-	pass
-	
+	var tween = create_tween()
+
+	# Fade screen to white in 0.3s
+	tween.tween_property(flash_rect, "color", Color(1, 1, 1, 1), 0.3)
+
+	# After fade, change scene
+	tween.tween_callback(Callable(self, "_do_scene_change")).set_delay(0.3)
+
+func _do_scene_change():
+	get_tree().change_scene_to_file(next_scene_path)
